7183cd1d169433d56b548c76f6ff1494767de128
Angi-Adema <dr.adema@gmail.com>
2026-03-24 17:23:53 -0600

    added the JTextField as well as three buttons, added the label, textField and buttons to the panel and the panel to the frame

--- src/bankaccountgui/BankAccountGUI.java
+++ src/bankaccountgui/BankAccountGUI.java
@@ -1,20 +1,43 @@
 package bankaccountgui;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class BankAccountGUI {
 
 	public static void main(String[] args) {
 		
-		// Create JFrame object for GUI window
+		// Create JFrame and JPanel objects for GUI window 
+		// and containers
 		JFrame frame = new JFrame("Bank Account");
+		JPanel panel = new JPanel();
 		
 		// Use a label to prompt the user to enter their
 		// current balance
-		JLabel label = new JLabel("Please enter your balance:");
+		JLabel label = new JLabel("Please enter amount:");
 		
-		 
+		// Add a text field and a submit button
+		JTextField textField = new JTextField(10);
+		JButton buttonSet = new JButton("Set Balance");
+		
+		// Add button to deposit
+		JButton buttonDep = new JBotton("Deposit");
+		
+		// Add button to withdraw
+		JButton buttonWit = new JButton("Withdraw");
+		
+		// Add items to the panel 
+		panel.add(label);
+		panel.add(textField);
+		panel.add(buttonSet);
+		panel.add(buttonDep);
+		panel.add(buttonWit);
+		
+		// Add the panel to the frame
+		frame.add(panel);
 
 	}
 
