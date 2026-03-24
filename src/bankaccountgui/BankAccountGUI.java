a7f54f8ea2b53393bcc46b7fd0da504968192b31
Angi-Adema <dr.adema@gmail.com>
2026-03-24 15:32:46 -0600

    created the frame and label objects to the GUI

--- src/bankaccountgui/BankAccountGUI.java
+++ src/bankaccountgui/BankAccountGUI.java
@@ -1,9 +1,20 @@
 package bankaccountgui;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 public class BankAccountGUI {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// Create JFrame object for GUI window
+		JFrame frame = new JFrame("Bank Account");
+		
+		// Use a label to prompt the user to enter their
+		// current balance
+		JLabel label = new JLabel("Please enter your balance:");
+		
+		 
 
 	}
 
